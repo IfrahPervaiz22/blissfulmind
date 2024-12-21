99e91df2c35dec53960cbce39f65ad3d9ccb9175
Ifrah Pervaiz <ifrahsyed529@gmail.com>
2024-12-21 22:27:09 +0500

    added routes

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
 
-pp = Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def my_function():
     return render_template('index.html')
+
+@app.route('/about')
+def my_function():
+    return render_template('about.html')
+
+@app.route('/gallery')
+def my_function():
+    return render_template('gallery.html')
+
+app.route('/servics')
+def my_function():
+    return render_template('service.html')
+
+
+@app.route('/login')
+def my_function():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
